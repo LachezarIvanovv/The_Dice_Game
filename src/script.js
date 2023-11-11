13feda8e28846dac97bb119545cf3efe56a11054
Lachezar Ivanov <128148925+LachezarIvanovv@users.noreply.github.com>
2023-11-11 15:18:01 +0200

    Added initial logic

---
+++ src/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0Element.textContent = currentScore;
+  } else {
+  }
+});
